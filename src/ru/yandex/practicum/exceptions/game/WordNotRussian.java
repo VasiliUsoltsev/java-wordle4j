aa458759304d8dd53a9ff1051eefb122d28fa158
VasiliUsoltsev <vasili.n.usoltsev@yandex.com>
2026-02-07 03:38:44 +0700

    1-я итерация 7 спринта - незначительные исправление по автотестам Git(несоответствие Code Style в классе исключении - WordNotRussian) - дубль 2

--- src/ru/yandex/practicum/exceptions/game/WordNotRussian.java
+++ src/ru/yandex/practicum/exceptions/game/WordNotRussian.java
@@ -1,8 +1,7 @@
 package ru.yandex.practicum.exceptions.game;
 
 public class WordNotRussian extends Exception {
-    public WordNotRussian(final String message)
-    {
+    public WordNotRussian(final String message) {
         super(message);
     }
 }
